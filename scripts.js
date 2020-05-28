703cb622bac7e70f266e06ec1bb9ffed4b240576
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-05-28 10:48:16 +0200

    feat(): new courses page

--- scripts.js
+++ scripts.js
@@ -1,29 +1,76 @@
-window.addEventListener('load', function() {
-  const newsletterAddButton = document.querySelector('.newsletter-add');
-  const newsletterEmailInput = document.querySelector('#newsletter-email');
-  const formRef = document.querySelector('form');
+window.addEventListener("load", function () {
+  var headerHeight = document.querySelector(".page-header").clientHeight;
+  var stickyNavbarElement = document.querySelector(".navbar-sticky");
+  var offset = 300;
+
+  toggleStickyNavbar();
+
+  window.addEventListener("scroll", function () {
+    toggleStickyNavbar();
+  });
+
+  function toggleStickyNavbar() {
+    if (window.scrollY > headerHeight - offset) {
+      return stickyNavbarElement.classList.add("visible");
+    }
+    stickyNavbarElement.classList.remove("visible");
+  }
+
+  registerNavigation();
+
+  function registerNavigation() {
+    var elements = document.querySelectorAll(".anchor");
+    elements.forEach(function (el) {
+      const href = el.getAttribute("href") || "";
+      const isAnchor = href[0] === "#";
+      if (!isAnchor) {
+        return;
+      }
+      el.addEventListener("click", function (event) {
+        if (!el.scrollIntoView) {
+          return;
+        }
+        event.preventDefault();
+
+        var targetElement = document.querySelector(el.getAttribute("href"));
+        if (targetElement) {
+          targetElement.scrollIntoView({ block: "start", behavior: "smooth" });
+        }
+      });
+    });
+  }
+
+  const newsletterAddButton = document.querySelector(".newsletter-form button");
+  const newsletterEmailInput = document.querySelector("#newsletter-email");
+  const formRef = document.querySelector(".newsletter-form form");
   if (newsletterAddButton && newsletterEmailInput) {
-    formRef.addEventListener('submit', function(e) {
+    formRef.addEventListener("submit", function (e) {
       e.preventDefault();
 
-      // styles
-      newsletterAddButton.setAttribute('disabled', 'disabled');
-      newsletterAddButton.classList.add('btn-disabled');
-      newsletterEmailInput.setAttribute('disabled', 'disabled');
+      newsletterAddButton.setAttribute("disabled", "disabled");
+      newsletterEmailInput.setAttribute("disabled", "disabled");
 
       const value = newsletterEmailInput.value;
       const xhr = new XMLHttpRequest();
       const url =
-        'https://z93f42xq2l.execute-api.us-east-2.amazonaws.com/Stage/newsletter?token=44168372b763b4b1d32093f6acfba88a';
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.onreadystatechange = function() {
-        newsletterAddButton.classList.remove('btn-disabled');
-        newsletterAddButton.classList.add('btn-success');
-        newsletterAddButton.textContent = 'Thank you';
+        "https://z93f42xq2l.execute-api.us-east-2.amazonaws.com/Stage/newsletter?token=db1f899025b5a59a76b6b34b2a013893";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onreadystatechange = function () {
+        newsletterAddButton.classList.add("btn-success");
       };
       const data = JSON.stringify({ email: value });
       xhr.send(data);
     });
   }
+
+  document
+    .querySelector(".category-heading")
+    .addEventListener("click", function (el) {
+      if (this.parentElement.classList.contains("opened")) {
+        this.parentElement.classList.remove("opened");
+      } else {
+        this.parentElement.classList.add("opened");
+      }
+    });
 });
